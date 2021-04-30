cac953c53806eab639bc35c41179b5a74da58906
antonio henrique <antoniohenriquedd15@hotmail.com>
2021-04-30 20:00:34 -0300

    funções de fila

--- arvore_binaria.c
+++ arvore_binaria.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 
 typedef struct No {
     int dado;
@@ -9,6 +9,17 @@ typedef struct No {
 } No;
 
 typedef No * p_no;
+
+typedef struct elem{
+    p_no no;
+    struct elem *seguinte
+} elem;
+
+typedef struct Fila {
+    elem *inicio, *fim;
+} Fila;
+
+typedef Fila *p_fila;
 
 p_no criar_arvore(int x) {
     p_no r = malloc( sizeof(No));
@@ -21,7 +32,6 @@ p_no criar_arvore(int x) {
 }
 
 p_no inserir(p_no raiz, int valor){
-
     if (raiz == NULL){
         raiz = (p_no)malloc(sizeof(No));
         raiz->dado = valor;
@@ -36,7 +46,6 @@ p_no inserir(p_no raiz, int valor){
 
         raiz->esq =  inserir(raiz->esq, valor);
     }
-
     return raiz;
 }
 
@@ -92,6 +101,97 @@ int altura(p_no raiz){
     return 1 + ( h_esq > h_dir ? h_esq : h_dir);
 }
 
+p_fila criar_fila(){
+    p_fila fila = malloc( sizeof(Fila));
+    fila->inicio = NULL;
+    fila->fim = NULL;
+    return fila;
+}
+
+bool fila_vazia(p_fila f){
+    if (f->inicio == NULL && f->fim == NULL){
+        return true;
+    }
+    return false;
+}
+
+void enfileira(p_fila fila, p_no raiz){
+    elem *p;
+    p = malloc( sizeof(elem));
+
+   // if (p == NULL){
+    //    printf('null\n');
+   // }
+    
+    p->no = raiz;
+    p->seguinte = NULL;
+    if (fila_vazia(fila)){
+        fila->inicio = p;
+        fila->fim = p;
+    }else{
+        fila->fim->seguinte = p;
+    }
+    fila->fim = p;
+}
+
+int tamanho(p_fila f){
+    elem *p;
+    int contador = 0;
+    p = f->inicio;
+    while (p != NULL)
+    {
+        contador++;
+        p = p->seguinte;
+    }
+    return contador;
+    
+}
+void mostrar_fila(p_fila f){
+    elem *p;
+    int tam = tamanho(f);
+    int inicio = 0;
+    p = f->inicio;
+    while (inicio < tam)
+    {
+        printf("%d\n", p->no->dado);
+        p = p->seguinte;
+        inicio++;
+    }
+}
+
+int desenfilerar(p_fila f){
+    elem *p;
+    if (!fila_vazia(f)){
+        
+        p = f->inicio;
+        f->inicio = p->seguinte;
+        if (f->inicio == NULL){
+            f->fim = NULL;
+        }
+        return p->no;
+    }else{
+        return NULL;
+    }
+}
+
+
+
+void percurso_em_largura(p_no raiz){
+    p_fila f;
+    f = criar_fila();
+    enfileira(f, raiz);
+    while(! fila_vazia(f)) {
+        raiz = desenfilerar(f);
+        if ( raiz != NULL) {
+            enfileira(f, raiz -> esq);
+            enfileira(f, raiz -> dir);
+            printf(" %d ", raiz -> dado);
+        }
+    }
+    printf("\n");
+}
+
+
 p_no criar_arvore(int x);
 
 p_no inserir(p_no raiz, int valor);
@@ -108,6 +208,18 @@ int numero_nos(p_no raiz);
 
 int altura(p_no raiz);
 
+void percurso_em_largura(p_no raiz);
+
+p_fila criar_fila();
+bool fila_vazia(p_fila f);
+
+void enfileira(p_fila fila, p_no raiz);
+int desenfilerar(p_fila fila);
+int tamanho(p_fila f);
+void mostrar_fila(p_fila f);
+
+
+
 void menu(){
     printf("1 - Criar arvore\n");
     printf("2 - Inserir na arvore\n");
@@ -117,12 +229,14 @@ void menu(){
     printf("6 - Visualizar arvore pré-ordem\n");
     printf("7 - Visualizar arvore pós-ordem\n");
     printf("8 - Visualizar arvore in-ordem\n");
+    printf("9 - Percuso em largura\n");
     printf("0 - Sair\n");
 }
 
 void menu();
 
 int main(){
+ 
     int entrada = 1;
     while (entrada != 0){
         int valor, numero, alt, *proc;
@@ -174,20 +288,18 @@ int main(){
                 in_ordem(arvore);
                 printf("--------------------------------------\n");
                 break;
+            case 9:
+                printf("----------------ARVORE----------------\n");
+                percurso_em_largura(arvore);
+                printf("--------------------------------------\n");
+                break;
             case 0:
 
                 entrada = 0;
                 break;
         }
     }
-   // p_no arvore = criar_arvore(5);
-   // p_no a = inserir(arvore, 7, 1);
-   // p_no b = inserir(arvore, 2, 1);
-   // p_no c = inserir(arvore, 6, 1);
-   // visualizar_arv(arvore);
-   // int x = numero_nos(arvore);
-   // printf("%d here\n", x);
-
-
+    
     return 0;
+
 }
